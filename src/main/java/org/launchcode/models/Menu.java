c5e429712ec4f0e34391d5b6a6c8889c0d1d9f85
Jorge Delgado <jgd2000@swbell.net>
2017-04-28 18:25:35 -0500

    3rd commit.  This is after 95% of part 3 is completed.  Having problem with error about "targetting an unmapped class: org.launchcode.models.Menu.cheeses[org.launchcode.models.Cheese]"

---
+++ src/main/java/org/launchcode/models/Menu.java
@@ -0,0 +1,53 @@
+package org.launchcode.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+/**
+ * Created by Jorge on 4/26/2017.
+ */
+@Entity
+public class Menu {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min = 3, max = 15)
+    private String name;
+
+    @ManyToMany
+    private List<Cheese> cheeses;
+
+    public List<Cheese> getCheeses() {
+        return cheeses;
+    }
+
+  //  public Menu(String name, List<Cheese> cheeses) {
+  //      this.name = name;
+  //      this.cheeses = cheeses;
+
+  //  }
+
+    public Menu() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void addItem(Cheese item){
+        cheeses.add(item);
+    }
+}
+
+
