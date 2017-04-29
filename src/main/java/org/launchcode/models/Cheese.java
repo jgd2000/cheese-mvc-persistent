4f4c909bf732f789adc91b614b18bd83dccda136
Jorge Delgado <jgd2000@swbell.net>
2017-04-29 16:28:18 -0500

    6th commit.  This is just an attempt to make sure all the latest file changes are pushed so that Github has the latest version of my files.

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,16 +1,14 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 /**
  * Created by LaunchCode
  */
-@Entity
+
 public class Cheese {
 
     @Id
@@ -28,6 +26,9 @@ public class Cheese {
 
     @ManyToOne
     private Category category;
+
+    @ManyToMany(mappedBy = "cheeses")
+    private List<Menu> menus;
 
     public Category getCategory() {
         return category;
--- src/main/java/org/launchcode/models/forms/AddMenuItemForm.java
+++ src/main/java/org/launchcode/models/forms/AddMenuItemForm.java
@@ -56,5 +56,5 @@ public class AddMenuItemForm {
     public Menu getMenu() {
         return menu;
     }
-   
+
 }
